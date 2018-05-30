2417e4d2c07d2934919bdb9e68933166d2079896
jwplayermurphy <32541714+jwplayermurphy@users.noreply.github.com>
2018-05-30 10:36:31 -0700

    Initial Commit
    Viant's new code repo for phase 2 of testing and their go-live efforts.

---
+++ moat-wrapped.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="https://z.moatads.com/jwplayerplugin0938452/moatplugin.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Dynamic Ad Playback from JSON Playlist</h2>
+				<hr>
+			</div>
+		</div>
+		<div style="margin: 0 auto; width: 1200px;">
+			<div style="width: 830px; padding: 15px; float: left; border-right:1px solid; height: 500px">
+				<h2>This is A Sweet Article</h2>
+				<p>Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+
+				<p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui.</p>
+
+				<p>Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+
+				<p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui.</p>
+
+				<p>Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+
+				<p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui.</p>
+			</div>
+			<div class="row" style="width: 320px; float: left; display: block; margin-left: 15px;">
+				<img src="https://thevideoink.com/wp-content/uploads/2017/05/0HElsOBJFvK7JBPem.png" width="100%">
+				<div class="col-md-6">	
+					<?php include "teads.html"; ?>
+				</div>
+			</div>
+		</div>	
+	</div>
+</body>
+<script>
+
+jwplayer('jwplayer_video_container').on('adImpression'), function(event){moatjw.add({partnercode:"viantjwplayer764158861377",player:this,adImpressionEvent:event})
+};
+	
+
+</script>
+</html>
